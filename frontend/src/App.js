f2ba37ab34826d6bd9262f78c25d1df2abb4b19d
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-03-02 21:58:09 +0545

    Globalcontext setup
    Globalcontext setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import { useGlobalContext } from './context/globalContext';
 function App() {
   const [active, setActive] = useState(1)
 
-  const global = useGlobalContext()
+  const global = useGlobalConxt()
   console.log(global);
 
   const displayData = () => {
