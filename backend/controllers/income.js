44eabdbb185530752a84ecbff2e783416fb57883
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-03-02 21:48:16 +0545

    income model setup
    income model setup

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -45,6 +45,6 @@ exports.deleteIncome = async (req, res) =>{
             res.status(200).json({message: 'Income Deleted'})
         })
         .catch((err) =>{
-            res.status(500).json({message: 'Server Error'})
+            res.status(500).json({message: 'Server Er'})
         })
 }
