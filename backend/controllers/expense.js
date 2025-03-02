0a70aa4f0764b81304498854afc6732a86ec5d27
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-03-02 21:47:02 +0545

    expense model setup
    expense model setup

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -45,6 +45,6 @@ exports.deleteExpense = async (req, res) =>{
             res.status(200).json({message: 'Expense Deleted'})
         })
         .catch((err) =>{
-            res.status(500).json({message: 'Server Error'})
+            res.status(500).json({message: 'Server Er'})
         })
 }
